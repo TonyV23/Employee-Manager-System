252162adba83d255e983eb318c7f6dfe1fed5936
viper23 <htonnycarlos@gmail.com>
2022-02-21 10:52:55 +0200

    added employee app

--- djangoWebapp/djangoWebapp/settings.py
+++ djangoWebapp/djangoWebapp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'Employee.apps.EmployeeConfig',
+    'rest_framework'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
