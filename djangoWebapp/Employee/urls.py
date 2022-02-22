71b5aa0e30d944f85383ad2a60d74ddc63cd605c
viper23 <htonnycarlos@gmail.com>
2022-02-22 09:07:30 +0200

    v

---
+++ djangoWebapp/Employee/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from Employee import views
+
+urlpatterns = [
+    url(r'^department/$', views.departmentApi),
+    url(r'^department/([0-9]+)$', views.departmentApi),
+    url(r'^employee/$', views.employeeApi),
+    url(r'^employee/([0-9]+)$', views.employeeApi)
+] 
--- djangoWebapp/Employee/views.py
+++ djangoWebapp/Employee/views.py
@@ -1,4 +1,3 @@
-import re
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -38,3 +37,33 @@ def departmentApi(request, id=0):
         department = Departments.objects.get(DepartmentId = id)
         department.delete()
         return JsonResponse ("Deleted Successfully !!", safe=False)        
+
+
+@csrf_exempt
+def employeeApi(request,id=0):
+    if request.method == 'GET':
+        employees = Employees.objects.all()
+        employees_serializer = EmployeeSerializer(employees, many=True)
+        return JsonResponse(employees_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        employee_data = JSONParser().parse(request)
+        employee_serializer = EmployeeSerializer(data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
+            return JsonResponse("Added Successfully!!" , safe=False)
+        return JsonResponse("Failed to Add.",safe=False)
+    
+    elif request.method == 'PUT':
+        employee_data = JSONParser().parse(request)
+        employee = Employees.objects.get(EmployeeId=employee_data['EmployeeId'])
+        employee_serializer = EmployeeSerializer(employee,data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
+            return JsonResponse("Updated Successfully!!", safe=False)
+        return JsonResponse("Failed to Update !!", safe=False)
+
+    elif request.method == 'DELETE':
+        employee = Employees.objects.get(EmployeeId=id)
+        employee.delete()
+        return JsonResponse("Deleted Succeffully!!", safe=False)
