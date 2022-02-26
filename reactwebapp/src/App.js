5c3f4d54ec9d3dcb6ad1010357f18e6733442fd8
viper23 <htonnycarlos@gmail.com>
2022-02-26 19:02:14 +0200

    added new components to app (home, department, employee, navigation)

--- reactwebapp/src/App.js
+++ reactwebapp/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       
+      <h3 className = "m-3 d-flex justify-content-center"> React Js</h3>
     </div>
   );
 }
