231fd0f1635bbcf3831a607912a450c232ad7066
viper23 <htonnycarlos@gmail.com>
2022-02-26 19:15:22 +0200

    added route to app.js

--- reactwebapp/src/App.js
+++ reactwebapp/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {Home} from "./Components/Home";
+import {Department} from "./Components/Department";
+import {Employee} from "./Components/Employee";
+import {Navigation} from "./Components/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      <h3 className = "m-3 d-flex justify-content-center"> React Js</h3>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h3 className = "m-3 d-flex justify-content-center"> React Js</h3>
+        <Navigation/>
+        <Switch>
+          <Route path="/" component ={Home} exact />
+          <Route path="/department" component ={Department} />
+          <Route path="/employee" component ={Employee} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
