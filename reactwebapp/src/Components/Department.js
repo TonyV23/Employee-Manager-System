5b9175c7cdee5105b229ef595dfd9daf834fd1b3
viper23 <htonnycarlos@gmail.com>
2022-02-26 23:36:01 +0200

    added table boostrap to dispaly departement's list

--- reactwebapp/src/Components/Department.js
+++ reactwebapp/src/Components/Department.js
@@ -1,12 +1,49 @@
 import React, {Component} from "react";
+import { Table } from "react-bootstrap";
 
 export class Department extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = { deps:[] }
+    }
+    
+    refreshList(){
+        fetch(process.env.REACT_APP_API+'department')
+        .then(response =>response.json())
+        .then(data =>{this.setState({deps:data})});
+    }
 
+    componentDidMount(){
+        this.refreshList();
+    }
+
+    componentDidUpdate(){
+        this.refreshList();
+    }
+    
     render(){
+        const {deps} = this.state;
 
         return(
-            <div className="mt-5 d-flex justify-content-left">
-                This is Department page
+            
+            <div >
+                <Table className="mt-4 " striped bordered hover size="sm">
+                    <thead>
+                        <th>DepartmentId</th>
+                        <th>DepartmentName</th>
+                        <th>Options</th>
+                    </thead>
+                    <tbody>
+                        {deps.map(dep=>
+                            <tr key={dep.DepartmentId}>
+                                <td>{dep.DepartmentId}</td>
+                                <td>{dep.DepartmentName}</td>
+                                <td>Edit / Delete</td>
+                            </tr>
+                            )}
+                    </tbody>
+                </Table>
             </div>
         )
     }
