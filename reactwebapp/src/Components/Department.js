0c4d0cd905a27d283d091f8c5e57cddd024167b0
viper23 <htonnycarlos@gmail.com>
2022-02-27 23:20:05 +0200

    added delete department modal feature

--- reactwebapp/src/Components/Department.js
+++ reactwebapp/src/Components/Department.js
@@ -25,6 +25,16 @@ export class Department extends Component {
         this.refreshList();
     }
     
+    deleteDep(depid){
+        if (window.confirm('Are you sure ?')){
+            fetch(process.env.REACT_APP_API + 'department/' + depid,{
+                method : "DELETE",
+                header : {"Accept" : "application/json",
+                          "Content-Type" : "application/json"
+                }
+            })
+        }
+    }
     render(){
         const {deps, depid, depname } = this.state;
 
@@ -48,6 +58,7 @@ export class Department extends Component {
                                 <td>
                                     <ButtonToolbar>
                                        <Button className = "mr-2" variant="info" onClick={()=> this.setState({editModalShow :true, depid : dep.DepartmentId, depname : dep.DepartmentName})}>Edit</Button>
+                                       <Button className = "mr-2" variant="danger" onClick={()=> this.deleteDep(dep.DepartmentId)}>Delete</Button>
                                        <EditDepModal show = { this.state.editModalShow } onHide = { editModalClose } depid = {depid} depname = {depname}/>
                                     </ButtonToolbar>
                                 </td>
